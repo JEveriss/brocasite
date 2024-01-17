deb9a22f59d055b46b90cd08c0281c0d3dbe3899
Jacques Everiss <jacques.everiss@gmail.com>
2024-01-17 16:35:22 +0000

    feat: added snap on scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import logo from '../assets/brocalogo.png'
+import Image from "next/image";
+import styles from "./page.module.css";
+import logo from "../assets/brocalogo.png";
+import Location from "./pages/location";
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.test}>
-        <Image className={styles.title} alt="broca Logo" src={logo}/>
-<hr/>
-        <Image className={styles.title} alt="broca Logo" src={logo}/>
-<hr/>
-        <Image className={styles.title} alt="broca Logo" src={logo}/>
-<hr/>
-        <Image className={styles.title} alt="broca Logo" src={logo}/>
-
+        <div className={styles.title}>
+          <Location />
+        </div>
+        <Image className={styles.title} alt="broca Logo" src={logo} />
+        <hr />
+        <Image className={styles.title} alt="broca Logo" src={logo} />
+        <hr />
+        <Image className={styles.title} alt="broca Logo" src={logo} />
+        <hr />
+        <Image className={styles.title} alt="broca Logo" src={logo} />
       </div>
     </div>
-  )
+  );
 }
